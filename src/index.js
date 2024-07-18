e135e52de5aef5776236828b8aa05c4bff6b5256
Erand <myftarierand@gmail.com>
2024-07-18 18:24:19 +0200

    script improvments

--- src/index.js
+++ src/index.js
@@ -63,10 +63,10 @@ async function createBanner(config) {
         `https://hexnlzzh3m.execute-api.eu-central-1.amazonaws.com/dev/api/v1/banners/space/${config.spaceId}`
     )
     const banners = await response.json()
-    const location = await getUserLocation()
+    const statistics = await getStats()
 
     if (Array.isArray(banners) && banners.length > 1) {
-        createSlider(config, banners, location)
+        createSlider(config, banners, statistics)
     } else {
         const content = document.createElement("div")
         content.style.width = "100%"
@@ -77,7 +77,12 @@ async function createBanner(config) {
             "display: block; width: 100%; height: 100%; object-fit: cover"
         image.setAttribute("data-id", banners[0].id)
         image.addEventListener("click", () =>
-            onBannerClick(banners[0].id, config.spaceId, location)
+            onBannerClick(
+                banners[0].id,
+                config.spaceId,
+                config.skinId,
+                statistics
+            )
         )
 
         content.appendChild(image)
@@ -90,7 +95,12 @@ async function createBanner(config) {
                 entries.forEach((entry) => {
                     if (entry.isIntersecting) {
                         const bannerId = entry.target.getAttribute("data-id")
-                        onBannerView(bannerId, config.spaceId, location)
+                        onBannerView(
+                            bannerId,
+                            config.spaceId,
+                            config.skinId,
+                            statistics
+                        )
                         observer.unobserve(entry.target)
                     }
                 })
@@ -101,32 +111,44 @@ async function createBanner(config) {
     }
 }
 
-function createSlider(config, banners, location) {
-    let currentIndex = 0
-    let interval
+function createSlider(config, banners, statistics) {
+    let interval,
+        currentIndex = 0
 
     const slider = document.createElement("div")
-    slider.style = "overflow: hidden;"
+    slider.style = "overflow: hidden; height: 300px; position: relative"
 
     const track = document.createElement("div")
-    track.style = "display: flex;transition: transform 0.5s ease;"
+    if (config.transition === "slide") {
+        track.style = "display: flex;transition: transform 0.5s ease;"
+    } else {
+        track.style = "position: relative; height: 100%;"
+    }
     slider.appendChild(track)
 
-    banners.forEach((banner) => {
+    banners.forEach((banner, index) => {
         const slide = document.createElement("img")
-        slide.className = "slide"
-        slide.style =
-            "display: block; width: 100%; height: 100%; object-fit: cover"
+        if (config.transition === "slide") {
+            slide.style =
+                "display: block; width: 100%; height: 100%; object-fit: cover"
+        } else {
+            slide.style =
+                "position: absolute; width: 100%; height: 100%; opacity: 0; transition: opacity 1s ease-in-out; object-fit: cover"
+
+            if (index === 0) slide.style.opacity = "1"
+        }
         slide.setAttribute("data-id", banner.id)
         slide.src = `http://betdemo.s3.eu-central-1.amazonaws.com/${banner.preview}`
         slide.addEventListener("click", () =>
-            onBannerClick(banner.id, config.spaceId, location)
+            onBannerClick(banner.id, config.spaceId, config.skinId, statistics)
         )
         track.appendChild(slide)
     })
 
-    const firstClone = track.firstElementChild.cloneNode(true)
-    track.appendChild(firstClone)
+    if (config.transition === "slide") {
+        const firstClone = track.firstElementChild.cloneNode(true)
+        track.appendChild(firstClone)
+    }
 
     const container = document.getElementById(config.containerId)
     container.appendChild(slider)
@@ -134,39 +156,167 @@ function createSlider(config, banners, location) {
     const slideWidth = track.firstElementChild.offsetWidth
 
     function moveToNextSlide() {
-        if (currentIndex >= banners.length) {
-            track.style.transition = "none"
-            currentIndex = 0
+        if (
+            config.infiniteLoop === "no" &&
+            currentIndex === banners.length - 2
+        ) {
+            stopSliding()
+        }
+
+        if (config.transition === "slide") {
+            if (currentIndex >= banners.length) {
+                track.style.transition = "none"
+                currentIndex = 0
+                track.style.transform = `translateX(-${
+                    currentIndex * slideWidth
+                }px)`
+                track.offsetHeight
+                track.style.transition = "transform 0.5s ease"
+            }
+
+            currentIndex++
             track.style.transform = `translateX(-${
                 currentIndex * slideWidth
             }px)`
-            track.offsetHeight
-            track.style.transition = "transform 0.5s ease"
+        } else {
+            const slides = track.childNodes
+            slides[currentIndex].style.opacity = "0"
+            currentIndex = (currentIndex + 1) % slides.length
+            slides[currentIndex].style.opacity = "1"
         }
-
-        currentIndex++
-        track.style.transform = `translateX(-${currentIndex * slideWidth}px)`
     }
 
+    if (config.navigation.includes("arrows")) {
+        function nextSlide() {
+            const slides = track.childNodes
+            const dots = document.getElementById("pagination").childNodes
+
+            slides[currentIndex].style.opacity = "0"
+            dots[currentIndex].style.backgroundColor = "transparent"
+            currentIndex = (currentIndex + 1) % slides.length
+            slides[currentIndex].style.opacity = "1"
+            dots[currentIndex].style.backgroundColor = "#fff"
+        }
+
+        function prevSlide() {
+            const slides = track.childNodes
+            const dots = document.getElementById("pagination").childNodes
+
+            slides[currentIndex].style.opacity = "0"
+            dots[currentIndex].style.backgroundColor = "transparent"
+            currentIndex = (currentIndex - 1 + slides.length) % slides.length
+            slides[currentIndex].style.opacity = "1"
+            dots[currentIndex].style.backgroundColor = "#fff"
+        }
+
+        const nextBtn = document.createElement("button")
+        nextBtn.innerHTML = `
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-chevron-right"
+            >
+                <path d="m9 18 6-6-6-6" />
+            </svg>
+        `
+        nextBtn.style =
+            "position: absolute; z-index: 2; top: 50%; transform: translateY(-50%); right: 0.5rem; display: grid; place-content: center; width: 2rem; aspect-ratio: 1; padding: 0; border: 0; background-color: transparent; color: #fff; cursor: pointer;"
+        const prevBtn = document.createElement("button")
+        prevBtn.innerHTML = `
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-chevron-left"
+        >
+            <path d="m15 18-6-6 6-6" />
+        </svg>
+    `
+        prevBtn.style =
+            "position: absolute; z-index: 2; top: 50%; transform: translateY(-50%); left: 0.5rem; display: grid; place-content: center; width: 2rem; aspect-ratio: 1; padding: 0; border: 0; background-color: transparent; color: #fff; cursor: pointer;"
+
+        nextBtn.addEventListener("click", nextSlide)
+        prevBtn.addEventListener("click", prevSlide)
+
+        slider.appendChild(nextBtn)
+        slider.appendChild(prevBtn)
+    }
+
+    function goToSlide(index) {
+        const dots = document.getElementById("pagination").childNodes
+        const slides = track.childNodes
+        slides[currentIndex].style.opacity = "0"
+        dots[currentIndex].style.backgroundColor = "transparent"
+        currentIndex = index
+        slides[index].style.opacity = "1"
+        dots[index].style.backgroundColor = "#fff"
+    }
+
+    if (config.navigation.includes("dots")) {
+        const slides = track.childNodes
+        const pagination = document.createElement("div")
+        pagination.id = "pagination"
+        pagination.style =
+            "display: flex; gap: 8px; position: absolute; left: 50%; transform: translateX(-50%); top: 1rem; z-index: 2;"
+
+        slides.forEach((_, index) => {
+            const dot = document.createElement("button")
+            dot.style =
+                "width: 10px; height: 10px; border: 1px solid #fff; border-radius: 50%; background-color: transparent; padding: 0; margin: 0;"
+            dot.addEventListener("click", () => goToSlide(index))
+            pagination.appendChild(dot, pagination)
+
+            if (index === 0) dot.style.backgroundColor = "#fff"
+        })
+
+        slider.appendChild(pagination)
+    }
+
+    const intervalTime = config.transitionSpeed
+        ? parseInt(config.transitionSpeed) * 1000
+        : 2000
+
     function startSliding() {
-        interval = setInterval(moveToNextSlide, 2000)
+        interval = setInterval(moveToNextSlide, intervalTime)
     }
 
     function stopSliding() {
         clearInterval(interval)
     }
 
-    slider.addEventListener("mouseenter", stopSliding)
-    slider.addEventListener("mouseleave", startSliding)
+    if (config.pauseOnHover === "yes") {
+        slider.addEventListener("mouseenter", stopSliding)
+        slider.addEventListener("mouseleave", startSliding)
+    }
 
-    startSliding()
+    if (config.autoplay === "yes") {
+        startSliding()
+    }
 
     const observer = new IntersectionObserver(
         (entries, observer) => {
             entries.forEach((entry) => {
                 if (entry.isIntersecting) {
                     const bannerId = entry.target.getAttribute("data-id")
-                    onBannerView(bannerId, config.spaceId, location)
+                    onBannerView(
+                        bannerId,
+                        config.spaceId,
+                        config.skinId,
+                        statistics
+                    )
                     observer.unobserve(entry.target)
                 }
             })
@@ -181,23 +331,15 @@ function createSlider(config, banners, location) {
         .forEach((banner) => observer.observe(banner))
 }
 
-async function onBannerView(bannerId, spaceId, location) {
-    const platform = getOS()
-    // const device = getDevice()
-    const browser = getBrowser()
-
+async function onBannerView(bannerId, spaceId, skinId, statistics) {
     const body = {
-        bannerId: bannerId,
-        spaceId: spaceId,
-        country: location.country,
-        city: location.city,
-        platform: platform,
-        browser: browser,
-        // device: device,
-        ip: location.query,
+        skinId,
+        spaceId,
+        bannerId,
+        ...statistics,
     }
 
-    if (bannerId && spaceId) {
+    if (bannerId && spaceId && skinId) {
         const headers = new Headers()
         headers.append("Content-Type", "application/json")
 
@@ -212,23 +354,15 @@ async function onBannerView(bannerId, spaceId, location) {
     }
 }
 
-async function onBannerClick(bannerId, spaceId, location) {
-    const platform = getOS()
-    // const device = getDevice()
-    const browser = getBrowser()
-
+async function onBannerClick(bannerId, spaceId, skinId, statistics) {
     const body = {
-        bannerId: bannerId,
-        spaceId: spaceId,
-        country: location.country,
-        city: location.city,
-        platform: platform,
-        browser: browser,
-        // device: device,
-        ip: location.query,
+        skinId,
+        spaceId,
+        bannerId,
+        ...statistics,
     }
 
-    if (bannerId && spaceId) {
+    if (bannerId && spaceId && skinId) {
         const headers = new Headers()
         headers.append("Content-Type", "application/json")
 
@@ -241,6 +375,38 @@ async function onBannerClick(bannerId, spaceId, location) {
             }
         )
     }
+}
+
+async function getStats() {
+    const platform = getOS()
+    const device = getDevice()
+    const browser = getBrowser()
+    const language = getLanguage()
+    const location = await getUserLocation()
+
+    return {
+        device,
+        browser,
+        platform,
+        language,
+        ip: location.query,
+        city: location.city,
+        country: location.country,
+    }
+}
+
+function getLanguage() {
+    const locale = navigator.language || navigator.userLanguage
+
+    if (locale) {
+        const languageNames = new Intl.DisplayNames(["en"], {
+            type: "language",
+        })
+
+        return languageNames.of(locale)
+    }
+
+    return "Other"
 }
 
 async function getUserLocation() {
@@ -267,7 +433,7 @@ function getOS() {
 }
 
 function getDevice() {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera
+    const userAgent = navigator.userAgent || window.opera
 
     if (/android/i.test(userAgent)) {
         return "Android Device"
